00771f93f048ce4263d13bc5051ff989b0e9d0f4
Abhi Govula <abhi.govula@scriptbees.com>
2017-08-14 14:35:27 +0530

    Initial commit

---
+++ src/components/emptyTest.spec.js
@@ -0,0 +1,11 @@
+// Must have at least one test file in this directory or Mocha will throw an error.
+// import NumberFormatter from './numberFormatter';
+
+describe('Number Formatter', () => {
+  describe('getCurrencyFormattedNumber', () => {
+    it('returns $5.50 when passed 5.5', () => {
+      // expect(1 == 1);
+    });
+  });
+
+});
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render } from 'react-dom'
+// import { Provider } from 'react-redux'
+// import { createStore } from 'redux'
+// import todoApp from './reducers'
+// import App from './components/App'
+
+// let store = createStore(todoApp)
+
+render(
+  <h1>jhd</h1>,
+  document.getElementById('app')
+);
